cd1dc5e23ac2d9be335775230ec02d25be852b55
unknown <juan.felipe.aliaga@gmail.com>
2021-04-19 16:16:40 -0300

    First Commit To Do List

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [title,setTitle] = useState('');
+  const [tasks,setTasks] = useState([])
+  const [tasksSaved,setTasksSaved] = useState([])
+
+  const addTask =()=>{
+      const createTask={
+      id: Math.random().toString(10).substring(7),
+      title: title,
+      }
+      if(title){
+        const tasksCopy = [...tasks];
+        tasksCopy.push(createTask);
+        console.log(tasksCopy)
+        setTasks(tasksCopy);
+        setTitle("");
+      }
+      saveTasks(tasks);
+      console.log(tasks)
+  }
+
+    const saveTasks =(tasks)=>{
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+    updateTask();
+ }
+
+ const updateTask=()=>{
+  setTasksSaved(JSON.parse(localStorage.getItem('tasks')));
+ }
+
+ useEffect(()=>{
+   updateTask();
+ },[tasks])
+
+ function deleteTask(id) {
+    const filteredTasks = tasks.filter((task) => task.id !== id);
+    localStorage.removeItem('tasks');
+    setTasks(filteredTasks);
+    saveTasks(tasks);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div>
+        <h3>TO DO LIST</h3>
+      </div>
+      <div className="input-container">
+        <input type="text" placeholder="Adicione uma tarefa" value={title} onChange={event =>setTitle(event.target.value)}></input>
+        <button className="btn-add"onClick={addTask}>ADICIONAR</button>
+      </div>
+      <div className="tasks-container">
+        {tasksSaved.map((task)=>(
+            <div className="task row" key={task.id}>
+              <div><p>{task.title}</p></div>
+              <div><button className="btn-cancel" onClick={()=>deleteTask(task.id)}>X</button></div>
+            
+          </div>
+        ))}
+        
+
+      </div>
+      
+      
     </div>
   );
 }
